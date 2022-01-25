4a60e917cd6b9738cf1a5ca5beed4e9d17e801f8
IN THE RA <79968570+inderashah007@users.noreply.github.com>
2022-01-25 11:04:58 +0800

    Create Userinfo.el

---
+++ Userinfo.el
@@ -0,0 +1,9 @@
+{
+    "sub": "auth0|123456789012345678901234",
+    "nickname": "johnkey",
+    "name": "inderashah007@gmail.com",
+    "picture": "https://gravatar.com/avatar/example.png",
+    "updated_at": "2018-05-07T14:16:52.013Z",
+    "email": "inderashah007@gmail.com",
+    "email_verified": "false"
+}
